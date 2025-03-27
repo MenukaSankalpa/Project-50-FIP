5ee035c8d2aa76a440aec094227465548f8aec94
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-03-27 11:48:19 +0530

    register parent and child details improve

--- register.php
+++ register.php
@@ -10,19 +10,19 @@ if($conn->connect_error) {
 
 //handle from submission 
 if($_SERVER["REQUEST_METHOD"] == "POST") {
-    $parent_id = $_POST[$parent_id ];
-    $child_name = $_POST[$child_name];
-    $email = $_POST[$email];
-    $address = $_POST[$address];
+    $parent_id = $_POST["parent_id" ];
+    $child_name = $_POST["child_name"];
+    $email = $_POST["email"];
+    $address = $_POST["address"];
 
     // prevent sql injection
     $parent_id = $conn->real_escape_string($parent_id);
-    $child_name = $conn->real_escape_string($$child_name);
-    $email = $conn->real_escape_string($$email);
-    $address = $conn->real_escape_string($$address);
+    $child_name = $conn->real_escape_string($child_name);
+    $email = $conn->real_escape_string($email);
+    $address = $conn->real_escape_string($address);
 
     //confirm if they exist for the Parent ID and the Name Child
-    $check_query = "SELECT * FROM parents WHERE parent_id = '$parent_id' AND child_name = '$$$child_name' ";
+    $check_query = "SELECT * FROM parents WHERE parent_id = '$parent_id' AND child_name = '$child_name' ";
     $result = $conn->query($check_query);
 
     if($result->num_rows > 0) {
@@ -34,9 +34,20 @@ if($_SERVER["REQUEST_METHOD"] == "POST") {
         } else {
             echo "Error Updating Record: " .$conn->error;
         }
+    } else {
+        //if record is not there insert new one
+        $insert_query = "INSERT INTO parents (parent_id, child_name, email, address) VALUES ('$parent_id', '$child_name', '$email', '$address')";
+        
+        
+        if($conn->query($insert_query) ===TRUE ) {
+            echo "Registration successfully!" ;
+        } else {
+            echo "Error: ".$insert_query . "<br>" .$conn->error;
+        }
     }
 
 }
 
-
+//close connection
+$conn->close();
 ?>
