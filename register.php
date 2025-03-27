a57f2fe98ff096df2fa9ba9b785bc759ec69a75c
MenukaSankalpa <menuka.sankalpa@gmail.com>
2025-03-27 09:46:11 +0530

    php part improve

--- register.php
+++ register.php
@@ -1,5 +1,14 @@
 <?php
 
 //Database Connection 
-$conn = new mysqli()
+$conn = new mysqli("localhost", "root", "", "school_admission");
+
+//check connection
+if($conn->connect_error) {
+    die("connection field: " . $conn->connect_error);
+}
+
+
+
+
 ?>
